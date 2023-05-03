15ac85a454c96d3537f1963f81551d15f6c3aba8
Selomon <soya.vandy1950@gmail.com>
2023-05-03 12:59:33 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: the list of the listint_t
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *ptr, *temp, *head;
+	unsigned int x, y;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	ptr = *h;
+	head = *h;
+	x = 0;
+
+	while (head != NULL)
+	{
+		temp = *h;
+		for (y = 0; y < x; y++)
+		{
+			if (temp == ptr)
+			{
+				*h = NULL;
+				return (x);
+			}
+			temp = temp->next;
+		}
+		ptr = head->next;
+		free(head);
+		head = ptr;
+		x++;
+	}
+	*h = NULL;
+	return (x);
+}
