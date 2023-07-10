d06c182417ab2015ca0423a42f33f324e6eab640
Selomon1 <soya.vandy1950@gmail.com>
2023-07-10 13:30:42 -0700

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	open_f = open(filename, O_RDWR | O_APPEND);
 	if (text_content != NULL)
-		for (str_len = 0; text_content[str_len]; str_len++)
+		for (str_len = 0, text_content[str_len], str_len++)
 			;
 	wri_f = write(open_f, text_content, str_len);
 	if (open_f == -1 || wri_f == -1)
@@ -25,4 +25,3 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(open_f);
 	return (1);
 }
-
